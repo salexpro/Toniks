cdeba60f5ce0c1ad495cba719ef63eadb9898514
Alexander <dev@salex.pro>
2018-05-07 01:56:37 +0300

    popup & form validation & map

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
-/* global Foundation */
+/* eslint no-unused-vars: off */
+/* global Foundation, initMap, google */
+
 /* 
 @codekit-prepend quiet '../../node_modules/jquery/dist/jquery.min',
 @codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min';
@@ -6,7 +8,9 @@
 @codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min';
 @codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.keyboard.min';
 @codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.triggers.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.motion.min';
 @codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.offcanvas.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.reveal.min';
 @codekit-prepend quiet '../../node_modules/owl.carousel/dist/owl.carousel.min';
 */
 
@@ -54,3 +58,28 @@ $(window).on('changed.zf.mediaquery', (event, newSize) => {
 })
 
 responsive(Foundation.MediaQuery.current);
+
+// Contact map
+function initMap() {
+    const latlng = {
+        lat: 48.1579592,
+        lng: 11.5852069
+    };
+
+    let center = {lat: 48.1579592, lng: 11.5852069};
+    if (Foundation.MediaQuery.current !== 'small') center.lat = 48.1609592
+
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: center,
+        disableDefaultUI: true
+    });
+
+    const marker = new google.maps.Marker({
+        position: latlng,
+        map: map
+    });
+}
+
+
+// Form submitting
