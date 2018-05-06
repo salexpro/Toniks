d499758c37697a022a5b9f7d892cce4a855c9992
Alexander <dev@salex.pro>
2018-05-06 23:33:36 +0300

    responsive main page

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+/* global Foundation */
 /* 
 @codekit-prepend quiet '../../node_modules/jquery/dist/jquery.min',
 @codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min';
@@ -6,14 +7,50 @@
 @codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.keyboard.min';
 @codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.triggers.min';
 @codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.offcanvas.min';
-@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.smoothScroll.min';
 @codekit-prepend quiet '../../node_modules/owl.carousel/dist/owl.carousel.min';
 */
 
 $(document).foundation();
 
+const menu = $('.head--fixed');
+$(window).scroll(() => {
+    if($(window).scrollTop() > 70){
+        menu.addClass('is_visible');
+    } else {
+        menu.removeClass('is_visible');
+    }
+});
+
 $('.owl-carousel').owlCarousel({
     items: 1,
     nav: false,
-    
+    responsive: {
+        0: {
+            items: 1
+        },
+        1024: {
+            items: 3
+        }
+    }
 });
+
+const offcanvas = $('.off-canvas');
+function responsive(size) {
+    if (size === 'small') {
+        offcanvas.removeClass('position-top').addClass('position-left');
+        $('.menu', offcanvas).addClass('vertical');
+        $('.button', offcanvas).removeClass('clear');
+        $('.close-button', offcanvas).addClass('small');
+    } else {
+        offcanvas.addClass('position-top').removeClass('position-left');
+        $('.menu', offcanvas).removeClass('vertical');
+        $('.button', offcanvas).addClass('clear');
+        $('.close-button', offcanvas).removeClass('small');
+    }
+}
+
+$(window).on('changed.zf.mediaquery', (event, newSize) => {
+    responsive(newSize);
+})
+
+responsive(Foundation.MediaQuery.current);
